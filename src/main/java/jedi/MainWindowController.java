353c8eef7517ce3b1d7054ee5df46558e1de3f77
Łukasz Paszko <lpaszko30@gmail.com>
2020-07-14 01:14:16 +0200

    dodano fileChooser ścieżki i pliki

--- src/main/java/jedi/MainWindowController.java
+++ src/main/java/jedi/MainWindowController.java
@@ -1,5 +1,6 @@
 package jedi;
 
+import java.io.File;
 import java.sql.SQLException;
 import java.util.Optional;
 
@@ -21,14 +22,20 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 
 public class MainWindowController {
 
 	private DBConnector 			dbConnector;
-    private Dialog<String[]> 		dialog;
+    private Dialog<String[]> 		inputDialog;
     
+    @FXML
+    private StackPane stackPane;
 	//JediOrder
     @FXML
     private ListView<String> 		joOrdersView;
@@ -50,6 +57,7 @@ public class MainWindowController {
     private Button 					joResetBtn;
     @FXML
     private Button 					joRegisterBtn;
+    private File 					joExpFile;
     
     //JediKnight
     @FXML
@@ -76,6 +84,7 @@ public class MainWindowController {
     @FXML
     private RadioButton 			rbDark;
     private ToggleGroup 			forceSide;
+    private File 					jkExpFile;
 	
 	
     @FXML
@@ -98,15 +107,13 @@ public class MainWindowController {
     	jkSaber.getItems().add("ró¿owy");
     	jkSaber.getItems().add("bia³y");
     	
-    	jkFilePath.setFocusTraversable(true);
-    	
     	forceSide = new ToggleGroup();   	
     	rbLight.setToggleGroup(forceSide);
     	rbDark.setToggleGroup(forceSide);
-    	dialog = new Dialog<>();
+    	inputDialog = new Dialog<>();
     	
-//    	setDialog();
-//    	Optional<String[]> result = dialog.showAndWait();
+//    	setInputDialog();
+//    	Optional<String[]> result = inputDialog.showAndWait();
 //    	result.ifPresent(connVals -> {
 //			try {
 //				establishConnection(connVals);
@@ -122,58 +129,16 @@ public class MainWindowController {
     	try {
 			establishConnection(new String[] {"5433","JediKnights","Delfin8765"});
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     }
     
-    private void setDialog() {
-		ButtonType connectBtn = new ButtonType("Po³¹cz", ButtonData.OK_DONE);
-		dialog.setTitle("Po³¹czenie z baz¹ danych");
-		dialog.setHeaderText("Wype³nij rekordy wymagane do po³¹czenia z baz¹ danych:\n nazwê, numer portu oraz has³o.");
-		dialog.getDialogPane().getButtonTypes().addAll(connectBtn, ButtonType.CANCEL);
-		GridPane grid = new GridPane();
-		grid.setHgap(10);
-		grid.setVgap(10);
-		grid.setPadding(new Insets(20,150,10,10));
-		TextField dbName = new TextField();
-		TextField portNum = new TextField();
-		PasswordField password = new PasswordField();
-		dbName.setPromptText("nazwa");
-		portNum.setPromptText("5432");
-		grid.add(new Label("Baza danych:"), 0, 0);
-		grid.add(dbName, 1, 0);
-		grid.add(new Label("Numer portu:"), 0, 1);
-		grid.add(portNum, 1, 1);
-		grid.add(new Label("Has³o:"), 0, 2);
-		grid.add(password, 1, 2);
-		
-		dialog.getDialogPane().setContent(grid);
-		
-		dialog.setResultConverter(dialogButton ->{
-			if(dialogButton == connectBtn) {
-				String[] ret = { portNum.getText(), dbName.getText(), password.getText() };
-				return ret;
-			}
-			return null;
-		});
-	}
-    
-    public void establishConnection(String[] connVals) throws SQLException {
-    	dbConnector = new DBConnector(this, connVals[0], connVals[1] , connVals[2]);
-		dbConnector.createTable("jedi_knights");
-		dbConnector.createTable("jedi_orders");
-		dbConnector.createTable("knights_orders");
-    }
-    
     @FXML
     void jkRegBtnOnAction(ActionEvent event) {
-    	
     	String name = jkName.getText();
     	String side = ((RadioButton)forceSide.getSelectedToggle()).getText();
     	String saber = jkSaber.getValue();
-    	int    power = (int) jediKnightPower.getValue();
-    	
+    	int    power = (int) jediKnightPower.getValue();   	
     	JediKnight jedi = new JediKnight(name, side, saber, power);
     	addKnightOrOrder(jedi);  	
     	dbConnector.insertInto(jedi);
@@ -181,14 +146,12 @@ public class MainWindowController {
     
     @FXML
     void jkFilePathOnMClicked(MouseEvent event) {
-    	System.out.println("ok");
+    	pickFile(event);
     }
     
     @FXML
     void joRegBtnOnAction(ActionEvent event) {
-    	
     	String name = joName.getText();
-    	
     	JediOrder order = new JediOrder(name);
     	addKnightOrOrder(order);
     	dbConnector.insertInto(order);
@@ -217,12 +180,79 @@ public class MainWindowController {
 		joOrders.set(indexOfOrder, order);
 	}
     
+    @FXML
+    void joFilePathOnMClicked(MouseEvent event) {
+    	pickFile(event);
+    }
+    
     public void addKnightOrOrder(Object knightOrOrder) {
     	if(knightOrOrder instanceof JediKnight) {
     		jkKnights.add(knightOrOrder.toString());
     		joKnights.add(((JediKnight)knightOrOrder).getName());
     	}else if(knightOrOrder instanceof JediOrder)
     		joOrders.add(knightOrOrder.toString());
+    }
+    
+    private void establishConnection(String[] connVals) throws SQLException {
+    	dbConnector = new DBConnector(this, connVals[0], connVals[1] , connVals[2]);
+		dbConnector.createTable("jedi_knights");
+		dbConnector.createTable("jedi_orders");
+		dbConnector.createTable("knights_orders");
+    }
+    
+    private void setInputDialog() {
+		ButtonType connectBtn = new ButtonType("Po³¹cz", ButtonData.OK_DONE);
+		inputDialog.setTitle("Po³¹czenie z baz¹ danych");
+		inputDialog.setHeaderText("Wype³nij rekordy wymagane do po³¹czenia z baz¹ danych:\n nazwê, numer portu oraz has³o.");
+		inputDialog.getDialogPane().getButtonTypes().addAll(connectBtn, ButtonType.CANCEL);
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(20,150,10,10));
+		TextField dbName = new TextField();
+		TextField portNum = new TextField();
+		PasswordField password = new PasswordField();
+		dbName.setPromptText("nazwa");
+		portNum.setPromptText("5432");
+		grid.add(new Label("Baza danych:"), 0, 0);
+		grid.add(dbName, 1, 0);
+		grid.add(new Label("Numer portu:"), 0, 1);
+		grid.add(portNum, 1, 1);
+		grid.add(new Label("Has³o:"), 0, 2);
+		grid.add(password, 1, 2);
+		
+		inputDialog.getDialogPane().setContent(grid);
+		
+		inputDialog.setResultConverter(dialogButton ->{
+			if(dialogButton == connectBtn) {
+				String[] ret = { portNum.getText(), dbName.getText(), password.getText() };
+				return ret;
+			}
+			return null;
+		});
+	}
+    
+    private void pickFile(MouseEvent event) {
+    	FileChooser fileChooser = new FileChooser();
+    	fileChooser.getExtensionFilters().add(new ExtensionFilter("Pliki tekstowe", "*.txt"));
+    	Stage stage = (Stage) stackPane.getScene().getWindow();
+    	if(event.getSource().hashCode() == joFilePath.hashCode()) {
+    		fileChooser.setTitle("Wybierz plik do ekspotru ZAKONÓW jedi");
+    		joExpFile = fileChooser.showOpenDialog(stage);
+        	if(joExpFile == null) {
+        		joFilePath.setText("");
+        		return;	
+        	}else
+        		joFilePath.setText(joExpFile.getAbsolutePath());
+    	}else if(event.getSource().hashCode() == jkFilePath.hashCode()) {
+    		fileChooser.setTitle("Wybierz plik do ekspotru RYCERZY jedi");
+    		jkExpFile = fileChooser.showOpenDialog(stage);
+        	if(jkExpFile == null) {
+        		jkFilePath.setText("");
+        		return;	
+        	}else
+        		jkFilePath.setText(jkExpFile.getAbsolutePath());
+    	}
     }
     
     public ObservableList<String> getJoOrders(){
